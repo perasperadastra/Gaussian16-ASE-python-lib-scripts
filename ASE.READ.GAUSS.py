ec68282bdadf54062ee39999e81b56fed6e1d190
Raul_D.A.R <67293033+perasperadastra@users.noreply.github.com>
2022-12-12 15:37:03 +0100

    Update ASE.READ.GAUSS.py

--- ASE.READ.GAUSS.py
+++ ASE.READ.GAUSS.py
@@ -16,26 +16,10 @@ fd = open(file_path,"r")
 # For the  L3MoN2MoL3_TZVP.log file 
 Glog = read_gaussian_out(fd)
 fd.close()
-#Gcell = Glog.get_cell()
-# Test if cell is not defined
-# if all index of the three vector of the cell are 0 it not set
-#if Gcell == 0:
-#	print(" no unit cell defined")
-#	print("copuld be defined with set_cell(a,b,c,A,B,C")
-# To obtain coordinate from and optimization (out)
-#Gfinal_Coord = Glog.get_positions()
-
-# To obtain initial charges
-#Gcharge_0 = Glog.get_initial_charges()
-
-# To obtain final charges (if avalible)
-#Gcharge_1 = Glog.get_charges()
-
-# In this example we will use the data obtained for the septuplete
-# 	To build a scan where N2 dissociates
 
 
-# se ha de crear un file-like opbject que es unh objieto tipico de python
+
+
 fd=open("./gaussian.com","w")
 
 write_gaussian_in(fd, #input file name to write
